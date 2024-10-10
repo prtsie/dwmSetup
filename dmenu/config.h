5a8796a1f727e656354f792f15c1806194b7bfcf
prtsie <yonhsup@mail.ru>
2024-10-10 20:20:08 +0300

    fuzzy

--- dmenu/config.h
+++ dmenu/config.h
@@ -2,20 +2,18 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"Hack:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	//Nord 
-//	[SchemeNorm] = { "#E5E9F0", "#3B4252" },
-//	[SchemeSel] = { "#E5E9F0", "#88C0D0" },
-//	[SchemeOut] = { "#000000", "#00ffff" },
-	
+	/*     fg         bg       */	
 	[SchemeNorm] = { "#f1be9b", "#020914" },
 	[SchemeSel] = { "#f1be9b", "#565879"},
+	[SchemeSelHighlight] = { "#ffc978", "#565879" },
+	[SchemeNormHighlight] = { "#ffc978", "#020914" },
 	[SchemeOut] = { "#f1be9b", "#DA6748" },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
